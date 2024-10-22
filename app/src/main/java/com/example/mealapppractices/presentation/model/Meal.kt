80fea4d5cf3adf25a8bc41ea79cd2a02f2097c3d
Roman <romamail.ru365@gmail.com>
2024-10-22 17:03:18 +0500

    meal model, mapper

---
+++ app/src/main/java/com/example/mealapppractices/presentation/model/Meal.kt
@@ -0,0 +1,32 @@
+package com.example.mealapppractices.presentation.model
+
+class Ingredient(
+  val ingredient: String,
+  val measure: String
+)
+
+data class Category(
+  val id: String,
+  val title: String,
+  val imgUrl: String,
+  val description: String
+)
+
+data class MealItem(
+  val id: String,
+  val title: String,
+  val imgUrl: String
+)
+
+data class MealItemDetails(
+  val id: String,
+  val title: String,
+  val drinkAlternate: String?,
+  val category: String,
+  val area: String,
+  val instructions: String,
+  val imgUrl: String,
+  val tags: List<String>,
+  val videoUrl: String,
+  val ingredients: List<Ingredient>
+)
